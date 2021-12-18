7806398e6a1e48005cca029be42a18ccf8defffe
zhiluniao <huangshunle_gxu@163.com>
2021-12-18 19:35:50 +0800

    functions

--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -2,7 +2,9 @@
 // Make me compile! Execute `rustlings hint functions1` for hints :)
 
 // I AM NOT DONE
-
+fn call_me(){
+    println!("call me phone number 911");
+}
 fn main() {
     call_me();
 }
